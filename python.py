565c643ea3c817a91915f83e828a59689e6e9109
Muklah Hilal <M-M.mexat@hotmail.com>
2018-12-21 23:08:49 +0300

    add comments

--- python.py
+++ python.py
@@ -1,13 +1,17 @@
 import math
 
+# method to checking each number
 def primeChecking(n, primes):
     if n in primes:
         return primes[n]
 
+# loop to check numbers if each of it is prime or not
     for num in range(2, math.floor(math.sqrt(n)+1)):
         if n % num == 0:
+          # if it's mod of 2 equal 0 then it's not prime and return false for that number in array of rotations number
             primes[n] = False
             return False
+   # if it's mod of 2 NOT equal 0 then it's prime and return true for that number in array of rotations number       
     primes[n] = True
     return True
 
@@ -17,16 +21,22 @@ def main():
     count = 0
     primes = {}
 
+# loop for numbers from 2 to 1000000
     for num in range (2, 1000000):
-        q = False
+        boolPrime = False
+
+        # change number from int to string
         num = str(num)
+
+        # loop to get all rotations of each number
         for i in range(len(num)):
             if primeChecking(int(num[i:]+num[:i]), primes):
-                q = True
+                boolPrime = True
             else:
-                q = False
+                boolPrime = False
                 break
-        if q:
+        # if all rotations of number are prime then increase counter by one
+        if boolPrime:
             count += 1
     print ("Number of prime numbers:",count)
 
